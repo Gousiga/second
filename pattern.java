e7973daae2320862f1c24927dea0aac80458e106
Gousiga <101163876+Gousiga@users.noreply.github.com>
2022-03-08 13:40:39 +0530

    project1

---
+++ pattern.java
@@ -0,0 +1,45 @@
+class pattern
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for( int j=1;j<=5;j++)
+{
+if(i==1||i==5)
+{
+System.out.print("1");
+}
+else
+{
+if(j==1||j==5)
+{
+System.out.print("1");
+}
+else
+{
+if(i==3&&j==3)
+{
+System.out.print("9");
+}
+else
+{
+
+
+
+
+System.out.print("0");}
+}
+}
+}
+System.out.println();
+
+
+
+}}}
+
+
+
+
+
+
