8304528e595fcfb9ccab411aa2023b59b3cbf0e5
Gousiga <101163876+Gousiga@users.noreply.github.com>
2022-03-08 20:40:43 +0530

    first

---
+++ maxmin.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class maxmin
+{
+public static void main(String Args[])
+{
+int a[]= {25,14,56,15,36,56,77,18,29,49};
+int max=a[0];
+int min=a[0];
+for(int i=0;i<a.length;i++)
+{
+if(a[i]>max){
+max=a[i];
+}
+if(a[i]<min){
+min=a[i];
+}}
+System.out.println("Original Array:  ");
+for(int i=0;i<a.length;i++){
+System.out.println(a[i]);
+}
+System.out.println("Maximum value for the above array: "+max);
+System.out.print("Minimum value for the above array: "+min);
+}}
